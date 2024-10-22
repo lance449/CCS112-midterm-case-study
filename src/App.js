4b619c6f0ba81cb718bd9490c456b857df352aec
Mark Joseph Malvar <149479021+itsmemarkmalvar@users.noreply.github.com>
2024-10-22 15:36:40 +0800

    Improve UI remove deprecated featues

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 // App.js
-import React from 'react';
+import React, { useRef } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import './components/AuthForm.css';
 
-
 function AnimatedRoutes() {
   const location = useLocation();
+  const nodeRef = useRef(null);
 
   return (
-    <TransitionGroup className="auth-container">
-      <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
-        <Routes location={location}>
-          <Route path="/" element={<RouteWrapper><Login /></RouteWrapper>} />
-          <Route path="/login" element={<RouteWrapper><Login /></RouteWrapper>} />
-          <Route path="/signup" element={<RouteWrapper><SignUp /></RouteWrapper>} />
-        </Routes>
-      </CSSTransition>
-    </TransitionGroup>
-  );
-}
-
-function RouteWrapper({ children }) {
-  return (
-    <div className="route-container">
-      {children}
+    <div className="auth-wrapper">
+      <TransitionGroup component={null}>
+        <CSSTransition
+          key={location.pathname}
+          classNames="fade"
+          timeout={300}
+          nodeRef={nodeRef}
+        >
+          <div ref={nodeRef} className="route-container">
+            <Routes location={location}>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
     </div>
   );
 }
